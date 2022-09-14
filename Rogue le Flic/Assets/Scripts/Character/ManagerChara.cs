7c08f70430be8bc42c1e5c1aa7fc7e89c9988a8c
Arthur <arthur.perigno@gmail.com>
2022-09-14 16:06:16 +0200

    Pleins de trucs randoms

--- Rogue le Flic/Assets/Scripts/Character/ManagerChara.cs
+++ Rogue le Flic/Assets/Scripts/Character/ManagerChara.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterManager : MonoBehaviour
+public class ManagerChara : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
--- Rogue le Flic/Assets/Scripts/Character/MovementsChara.cs
+++ Rogue le Flic/Assets/Scripts/Character/MovementsChara.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterMovements : MonoBehaviour
+public class MovementsChara : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
