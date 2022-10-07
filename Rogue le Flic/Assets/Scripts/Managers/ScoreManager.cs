316067cb0fe52fbc71532934d341d9a7e252795f
GONEN <e.gonen@rubika-edu.com>
2022-10-07 13:07:03 +0200

    TextMesh et Score

--- Rogue le Flic/Assets/Scripts/Managers/ScoreManager.cs
+++ Rogue le Flic/Assets/Scripts/Managers/ScoreManager.cs
@@ -1,13 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class ScoreManager : MonoBehaviour
 {
+    public TextMeshPro scoreText; 
+    public Text highscoreText;
+
+    private int score = 0;
+    private int highscore = 0;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        scoreText.text = score.ToString() + "Points";
+        highscoreText.text = "Highscore: " + highscore.ToString();
     }
 
     // Update is called once per frame
