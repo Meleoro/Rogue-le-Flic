7683baec49b16d837e953f0a0609ebcdcb4dc2ef
Arthur <arthur.perigno@gmail.com>
2022-11-22 16:56:23 +0100

    Début shop

--- Rogue le Flic/Assets/Scripts/Shop.cs
+++ Rogue le Flic/Assets/Scripts/Shop.cs
@@ -1,27 +1,75 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Shop : MonoBehaviour
 {
+    [SerializeField] private RectTransform shopkeeperUI;
+
+    [Header("Ouverture Shop")] 
+    [SerializeField] private Vector2 posShopkeeper;
+    [SerializeField] private float openingDuration;
+
+    [Header("Fermeture Shop")]
+    private Vector2 originalPosShopkeeper;
+    [SerializeField] private float closingDuration;
+
+    [Header("Others")] 
+    private bool isOpen;
+    private bool canUseShop;
 
 
-    public GameObject shopMenu;
-    
-    // Update is called once per frame
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void Start()
     {
-        if (collision.CompareTag("Player"))
+        originalPosShopkeeper = shopkeeperUI.localPosition;
+    }
+
+    
+    private void Update()
+    {
+        if (ManagerChara.Instance.controls.Character.Enter.WasPressedThisFrame() && canUseShop)
         {
-            shopMenu.SetActive(true);
+            if(!isOpen)
+                OpenShop();
+            
+            else
+                CloseShop();
         }
     }
 
-    private void OnTriggerExit2D(Collider2D collision)
+
+    void OpenShop()
     {
-        if (collision.CompareTag("Player"))
+        shopkeeperUI.DOLocalMove(posShopkeeper, openingDuration);
+        ManagerChara.Instance.noControl = true;
+
+        isOpen = true;
+    }
+
+    void CloseShop()
+    {
+        shopkeeperUI.DOLocalMove(originalPosShopkeeper, closingDuration);
+        ManagerChara.Instance.noControl = false;
+
+        isOpen = false;
+    }
+    
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
         {
-            shopMenu.SetActive(false);
+            canUseShop = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canUseShop = false;
         }
     }
 }
