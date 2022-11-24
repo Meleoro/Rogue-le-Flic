0cf353955ae2a6d8a7a9847c06f76c6ae6fc5cd7
GONEN <e.gonen@rubika-edu.com>
2022-11-24 20:31:56 +0100

    Scene testable

--- Rogue le Flic/Assets/Scripts/ScriptTest/Activation.cs
+++ Rogue le Flic/Assets/Scripts/ScriptTest/Activation.cs
@@ -16,7 +16,7 @@ public class Activation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P))
+        if (Input.GetKeyDown(KeyCode.Keypad9))
         {
             gameobject.SetActive(true);
         }
