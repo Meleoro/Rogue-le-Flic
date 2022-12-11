6be1023518bc169bc2b4d8b7483dfdfd05e4acff
GONEN <e.gonen@rubika-edu.com>
2022-12-11 19:38:03 +0100

    ScoreManager

--- Rogue le Flic/Assets/Scripts/Managers/ScoreManager.cs
+++ Rogue le Flic/Assets/Scripts/Managers/ScoreManager.cs
@@ -10,12 +10,22 @@ public class ScoreManager : MonoBehaviour
 {
     public static ScoreManager instance;
     
-    public TMP_Text scoreText;
-    public TMP_Text highscoreText;
+    public TMP_Text scoreActuelText;
+    public TMP_Text scoreCountText;
+    public TMP_Text timerText;
+    public TMP_Text enemyKillTxt;
+
+
+
+    public int scoreActuel;
+    public int scoreCount;
+    public int enemyKill;
+    public float timer;
+    public int scoreAdd = 100;
+
+    public int timerMax = 5;
     
     
-    private int score = 0;
-    private int highscore = 0;
 
     private void Awake()
     {
@@ -25,17 +35,55 @@ public class ScoreManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        scoreText.text = score.ToString() + " Points";
-        highscoreText.text = "Highscore: " + highscore.ToString();
-        Debug.Log("score =" + score);
-
+        scoreActuel = 0;
+        scoreCountText.alpha = 0;
+        enemyKillTxt.alpha = 0;
+        timerText.alpha = 0;
+        enemyKill = 1;
+        timer = timerMax;
     }
 
-    public void AddPoint()
+
+    private void Update()
     {
-        score += 100;
-        scoreText.text = score.ToString() + " Points";
-        Debug.Log("score =" + score);
+        scoreCountText.text = scoreCount.ToString();
+        scoreActuelText.text = scoreActuel.ToString();
+        enemyKillTxt.text = enemyKill.ToString();
+        
+
+        timer = timer - 0.005f;
+        
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            EnemyKilled();
+        }
+
+
+        if (timer < 0)
+        {
+            NoTimeLeft();
+        }
+        Debug.Log(timer);
     }
-    
+
+    public void EnemyKilled()
+    {
+        scoreCountText.alpha = 255;
+        timerText.alpha = 255;
+        scoreCount = scoreCount + scoreAdd*enemyKill;
+        timer = 5;
+        enemyKill = enemyKill + 1;
+        enemyKillTxt.alpha = 255;
+
+    }
+
+
+    public void NoTimeLeft()
+    {
+        scoreActuel = scoreActuel + scoreCount;
+        scoreCount = 0;
+        scoreCountText.alpha = 0;
+        timerText.alpha = 0;
+    }
+
 }
