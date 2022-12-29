34df66353e103e579c930be5bb4124dda33074dc
Arthur <arthur.perigno@gmail.com>
2022-12-29 11:21:12 +0100

    Début boss beaver

---
+++ Rogue le Flic/Assets/BeaverBoss.cs
@@ -0,0 +1,141 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class BeaverBoss : MonoBehaviour
+{
+    [Header("Movement Speeds")]
+    public float speedX;
+    public float speedY;
+
+    [Header("Deceleration")]
+    public float dragDeceleration;
+    public float dragMultiplier;
+
+    [Header("Castor")]
+    [SerializeField] private int health;
+    [SerializeField] private float cooldownMin;
+    [SerializeField] private float cooldownMax;
+    private float timer;
+    private bool isAttacking;
+    private bool canMove;
+    private int currentAttack;
+
+    [Header("Charge")]
+    [SerializeField] private float distanceJumpTrigger;
+    [SerializeField] private float strenghtJump;
+    private bool isCharging;
+
+    [Header("Spawn")]
+    [SerializeField] private int minCastorSpawn;
+    [SerializeField] private int maxCastorSpawn;
+    [SerializeField] private GameObject castor;
+
+    [Header("GigaCharge")]
+    [SerializeField] private float strenghtGigaJump;
+    private bool isGigaCharging;
+
+    [Header("References")]
+    public AIPath AIPath;
+    public AIDestinationSetter AIDestination;
+    private Rigidbody2D rb;
+    private Boss boss;
+
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.drag = dragDeceleration * dragMultiplier;
+
+        canMove = true;
+
+        AIDestination.target = ManagerChara.Instance.transform;
+
+        boss = GetComponent<Boss>();
+
+        timer = Random.Range(cooldownMin, cooldownMax);
+        currentAttack = 0;
+    }
+
+
+    public void BeaverBehavior()
+    {
+        if (!isAttacking)
+        {
+            timer -= Time.deltaTime;
+            
+            if (timer <= 0)
+            {
+                isAttacking = true;
+
+                currentAttack = Random.Range(1, 4);
+            }
+        }
+
+        if (isAttacking)
+        {
+            // CHARGE
+            if (currentAttack == 1)
+            {
+                StartCoroutine(Charge(new Vector2(AIPath.destination.x - transform.position.x, AIPath.destination.y - transform.position.y)));
+            }
+
+            // SPAWN
+            else if (currentAttack == 2)
+            {
+                StartCoroutine(Spawn());
+            }
+
+            // GIGA CHARGE
+            else 
+            {
+                StartCoroutine(GigaCharge(new Vector2(AIPath.destination.x - transform.position.x, AIPath.destination.y - transform.position.y)));
+            }
+        }
+    }
+
+
+
+    IEnumerator Charge(Vector2 directionJump)
+    {
+        isCharging = true;
+
+        rb.AddForce(-directionJump.normalized * (strenghtJump / 5), ForceMode2D.Impulse);
+
+        yield return new WaitForSeconds(0.75f);
+
+        rb.AddForce(directionJump.normalized * strenghtJump, ForceMode2D.Impulse);
+
+        yield return new WaitForSeconds(0.25f);
+
+        isAttacking = false;
+        isGigaCharging = true;
+        timer = Random.Range(cooldownMin, cooldownMax);
+    }
+
+
+    IEnumerator Spawn()
+    {
+        yield return new WaitForSeconds(1);
+    }
+
+
+    IEnumerator GigaCharge(Vector2 directionJump)
+    {
+        isGigaCharging = true;
+
+        rb.AddForce(-directionJump.normalized * (strenghtJump / 5), ForceMode2D.Impulse);
+
+        yield return new WaitForSeconds(0.75f);
+
+        rb.AddForce(directionJump.normalized * strenghtJump, ForceMode2D.Impulse);
+
+        yield return new WaitForSeconds(0.25f);
+
+        isAttacking = false;
+        isGigaCharging = false;
+        timer = Random.Range(cooldownMin, cooldownMax);
+    }
+}
---
+++ Rogue le Flic/Assets/Boss.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    public enum boss
+    {
+        Beaver,
+        Frog,
+        Turtle
+    }
+
+    public boss bossType;
+
+    private Beaver beaverScript;
+    private Frog frogScript;
+    private Turtle turtleScript;
+
+    [Header("References")]
+    public Animator anim;
+    public GameObject sprite;
+
+
+    private void Start()
+    {
+        switch (bossType)
+        {
+            case boss.Beaver:
+                beaverScript = GetComponent<Beaver>();
+                break;
+
+            case boss.Frog:
+                frogScript = GetComponent<Frog>();
+                break;
+
+            case boss.Turtle:
+                turtleScript = GetComponent<Turtle>();
+                break;
+        }
+    }
+
+
+    private void Update()
+    {
+        switch (bossType)
+        {
+            case boss.Beaver:
+                beaverScript.BeaverBehavior();
+                break;
+        }
+    }
+}
