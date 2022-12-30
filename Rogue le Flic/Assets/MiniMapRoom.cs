0df774ef572126471137379732765a88fdebb18c
Arthur <arthur.perigno@gmail.com>
2022-12-30 16:59:58 +0100

    Mini map doors

---
+++ Rogue le Flic/Assets/MiniMapRoom.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapRoom : MonoBehaviour
+{
+    public GameObject rightDoor;
+    public GameObject leftDoor;
+    public GameObject upDoor;
+    public GameObject downDoor;
+}
--- Rogue le Flic/Assets/Scripts/Managers/MiniMapManager.cs
+++ Rogue le Flic/Assets/Scripts/Managers/MiniMapManager.cs
@@ -37,12 +37,58 @@ public class MiniMapManager : MonoBehaviour
                 if (activeMap.list[i].list[k] != null)
                 {
                     Vector2 newPos = origin + new Vector3(i, k, 0);
-                    
-                    activeRooms.Add(Instantiate(room, newPos, Quaternion.identity));
+
+                    GameObject newRoom = Instantiate(room, newPos, Quaternion.identity);
+
+                    activeRooms.Add(newRoom);
+
+                    ActivateDoors(i, k, newRoom);
                 }
             }
         }
 
         cameraMini.transform.position = origin + new Vector3(MapManager.Instance.playerX, MapManager.Instance.playerY, -10);
+    }
+
+    
+    void ActivateDoors(int x, int y, GameObject miniMapRoom)
+    {
+        MiniMapRoom script = miniMapRoom.GetComponent<MiniMapRoom>();
+
+        if (GenerationPro.Instance.map.list[x + 1].list[y] != null)
+        {
+            script.rightDoor.SetActive(true);
+        }
+        else
+        {
+            script.rightDoor.SetActive(false);
+        }
+
+        if (GenerationPro.Instance.map.list[x].list[y - 1] != null)
+        {
+            script.downDoor.SetActive(true);
+        }
+        else
+        {
+            script.downDoor.SetActive(false);
+        }
+
+        if (GenerationPro.Instance.map.list[x].list[y + 1] != null)
+        {
+            script.upDoor.SetActive(true);
+        }
+        else
+        {
+            script.upDoor.SetActive(false);
+        }
+
+        if (GenerationPro.Instance.map.list[x - 1].list[y] != null)
+        {
+            script.leftDoor.SetActive(true);
+        }
+        else
+        {
+            script.leftDoor.SetActive(false);
+        }
     }
 }
