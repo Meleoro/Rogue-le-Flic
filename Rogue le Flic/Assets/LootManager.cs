3acc951bba183386de4565f3ee4ffa0f1fa8c238
Arthur <arthur.perigno@gmail.com>
2023-01-19 13:41:25 +0100

    Pas de doublons en modules

--- Rogue le Flic/Assets/LootManager.cs
+++ Rogue le Flic/Assets/LootManager.cs
@@ -38,6 +38,12 @@ public class LootManager : MonoBehaviour
     [Range(0, 100)] public int probaModuleLvl2;
     [Range(0, 100)] public int probaModuleLvl3;
 
+    [Header("Other")] 
+    private bool weaponChosen;
+    private bool moduleChosen;
+    private int currentModule1;
+    private int currentModule2;
+    
 
     private void Awake()
     {
@@ -144,19 +150,35 @@ public class LootManager : MonoBehaviour
     {
         int index = Random.Range(0, 100);
 
+        int moduleSelected = 0;
+        moduleChosen = false;
+
+        currentModule1 = ModuleManager.Instance.Module1;
+        currentModule2 = ModuleManager.Instance.Module2;
+
+        while (!moduleChosen)
+        {
+            moduleSelected = Random.Range(1, 6);
+
+            if (moduleSelected != currentModule1 && moduleSelected != currentModule2)
+            {
+                moduleChosen = true;
+            }
+        }
+
         if (index < probaModuleLvl1)
         {
-            return modulesLevel1[Random.Range(0, modulesLevel1.Count)];
+            return modulesLevel1[moduleSelected - 1];
         }
         
         else if (index < probaModuleLvl1 + probaModuleLvl2)
         {
-            return modulesLevel2[Random.Range(0, modulesLevel2.Count)];
+            return modulesLevel2[moduleSelected - 1];
         }
         
         else
         {
-            return modulesLevel3[Random.Range(0, modulesLevel3.Count)];
+            return modulesLevel3[moduleSelected - 1];
         }
     }
     
