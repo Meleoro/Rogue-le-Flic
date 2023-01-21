f034279fe7ab919d02dd1fd3b6610c51fc7029be
Arthur <arthur.perigno@gmail.com>
2023-01-21 19:08:00 +0100

    HUD Stylé pour le gun

--- Rogue le Flic/Assets/Scripts/HUDManager.cs
+++ Rogue le Flic/Assets/Scripts/HUDManager.cs
@@ -4,13 +4,15 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class HUDManager : MonoBehaviour
 {
     public static HUDManager Instance;
 
     [SerializeField] private GameObject ammoUI;
-    [SerializeField] private Image gunImage;
+    public Image gunImage;
+    public RectTransform gunImagePos;
     public TextMeshProUGUI ammo;
 
     private Vector2 normalPos;
@@ -44,7 +46,10 @@ public class HUDManager : MonoBehaviour
 
         gunImage.sprite = sprite;
         gunImage.SetNativeSize();
-        
+    }
+
+    public void ReplaceImage(int maxAmmo)
+    {
         if (maxAmmo == 1)
         {
             gunImage.rectTransform.localPosition = bowPos;
@@ -54,5 +59,8 @@ public class HUDManager : MonoBehaviour
         {
             gunImage.rectTransform.localPosition = normalPos;
         }
+        
+        gunImagePos.DOLocalMoveY(gunImagePos.localPosition.y + 8, 0.1f).OnComplete((() =>
+            gunImagePos.DOLocalMoveY(gunImagePos.localPosition.y - 8, 0.1f)));
     }
 }
